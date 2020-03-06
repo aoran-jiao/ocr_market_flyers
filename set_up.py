45b485f7f799f857c4547f59e632e884d0ed66a6
Jack <40149895+jack666666666666@users.noreply.github.com>
2020-03-05 22:05:19 -0500

    Create set_up.py

---
+++ set_up.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+try:
+  # %tensorflow_version only exists in Colab.
+  %tensorflow_version 2.x
+except Exception:
+  pass
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import figure
+
